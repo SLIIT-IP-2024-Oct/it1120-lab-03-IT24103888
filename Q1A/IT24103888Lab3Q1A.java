8c3531f757bb21a272c8502278e940ee67684555
IT24103888 <it24103888@my.sliit.lk>
2024-11-01 09:26:50 +0530

    The total amount I have to pay

---
+++ Q1A/IT24103888Lab3Q1A.java
@@ -0,0 +1,16 @@
+public class IT24103888Lab3Q1A {
+
+    public static void main(String[] args) {
+    // Price of 1kg of rice
+    int price = 225;
+    // Number of kilograms I want to buy
+    int kilograms = 5;
+    int amount;
+
+    // Calculation
+    amount = price*kilograms;
+
+    // Output the results
+    System.out.println("The Total amount is:" +amount);
+    }
+}
